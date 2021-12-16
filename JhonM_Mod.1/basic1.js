4e30567bfc06249c21bb509efaf6548459e91bd6
BernardoVindex <ber.cardenas94@gmail.com>
2021-12-16 00:27:22 -0600

    Update 1

--- JhonM_Mod.1/basic1.js
+++ JhonM_Mod.1/basic1.js
@@ -1,1 +1,7 @@
 // 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. miFuncion("Hola Mundo") devolverá 10.
+
+const characterCounter = string => {
+    return string.length
+}
+
+characterCounter()
