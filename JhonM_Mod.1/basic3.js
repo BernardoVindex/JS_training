eec6fe274f100b2fff041038ab7f54c070e15c64
BernardoVindex <ber.cardenas94@gmail.com>
2021-12-21 16:45:44 -0600

    Fist excercises

--- JhonM_Mod.1/basic3.js
+++ JhonM_Mod.1/basic3.js
@@ -1,1 +1,27 @@
 // 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+
+const addCharact = function(string, separator) {
+  const charChached = []
+  const stringCached = []
+    
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== separator) {
+      charChached.push(string[i])  
+    } else {
+      stringCached.push(charChached.join(''))
+      charChached.splice(0, charChached.length)
+    }
+    if (i == string.length-1) {
+      stringCached.push(charChached.join(''))
+    }
+
+  }
+  
+  return stringCached
+  
+}
+
+console.log(addCharact('para poder mejorar en javascript', ' '))
+
+// console.log(addCharact('abcde', 1))
+
