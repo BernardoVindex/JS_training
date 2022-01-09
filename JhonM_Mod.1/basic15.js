58ba9f3254bb4940effdb5cbdaf74521a0e1cc0d
BernardoVindex <ber.cardenas94@gmail.com>
2022-01-09 16:43:47 -0600

    avan

--- JhonM_Mod.1/basic15.js
+++ JhonM_Mod.1/basic15.js
@@ -1,1 +1,13 @@
 // 15) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.
+
+const binaryToDecimal = (number, mode) => {
+  if (!number) return console.warn('No has escrito ningún número')
+  if (!mode) return console.warn('EL tipo de número que es (decimál: 10 o binario: 2)')
+  
+  
+  return (mode === 10)
+            ? console.info(`El número decimál "${number}" en base binaria es: ${number.toString(2)}`)
+            : console.info(`EL dígito binario "${number}" en base decimal es: ${parseInt(number, 2)}`)
+}
+
+binaryToDecimal(4, 10)
--- JhonM_Mod.1/basic16.js
+++ JhonM_Mod.1/basic16.js
@@ -1,1 +1,7 @@
 // 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
+
+const blackFrayday = (price, desc) => {
+  return console.info(`Con el descuento a"$${price}" el precio queda en: $${price * ( 1 - (desc / 100))}`)
+}
+
+blackFrayday(1000,20)
