1547298e5dbf77df0176d10e3f65c9fa8a580c3d
BernardoVindex <ber.cardenas94@gmail.com>
2022-01-15 12:34:14 -0600

    Class

--- JhonM_Mod.1/basic27.js
+++ JhonM_Mod.1/basic27.js
@@ -1,9 +1,8 @@
 // 27) Programa una clase llamada Pelicula.
 
 // <!-- 27) Programa una clase llamada Pelicula. -->
-
-
 // <!-- La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
+
 //   - Todos los datos del objeto son obligatorios.
 //   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
 //      7 restantes números.
@@ -23,3 +22,74 @@
 //     de cada película.
 
 // * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western. -->
+
+const filmoteca = [
+  [
+    id,
+    titulo,
+    director,
+    añoEstreno,
+    paisOrigen,
+    género,
+    calificacion
+ ],
+  [
+    id,
+    titulo,
+    director,
+    añoEstreno,
+    paisOrigen,
+    género,
+    calificacion
+  ],
+  [
+    id,
+    titulo,
+    director,
+    añoEstreno,
+    paisOrigen,
+    género,
+    calificacion
+  ]
+
+]
+
+const generosDisponibles = [
+  'Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film Noir', 'Game'-'Show', 'History', 'Horror', 'Musical', 'Music', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk'-'Show', 'Thriller', 'War', 'Western'
+]
+
+class Pelicula {
+  constructor ({
+    id,
+    titulo,
+    director,
+    añoEstreno,
+    paisOrigen,
+    género,
+    calificacion
+  })
+
+  {this.id = id
+  this.titulo = titulo
+  this.director = director
+  this.añoEstreno = añoEstreno
+  this.paisOrigen = paisOrigen
+  this.género = género
+  this.calificacion = calificacion}
+
+  static generos () {
+    
+  }  
+
+  ficha () {
+    console.log(`
+      Id: ${this.id}
+      Titulo: ${this.titulo}
+      Director: ${this.director}
+      Año de Estreno: ${this.añoEstreno}
+      Pais de Origen: ${this.paisOrigen}
+      Género: ${this.género} 
+      Calificación: ${this.calificacion} 
+    `)
+  }
+}
