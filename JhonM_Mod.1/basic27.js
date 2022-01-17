085e7cac941b6626872fe4c00ad90eaa1e0beec6
BernardoVindex <ber.cardenas94@gmail.com>
2022-01-17 17:02:02 -0600

    The last

--- JhonM_Mod.1/basic27.js
+++ JhonM_Mod.1/basic27.js
@@ -23,73 +23,154 @@
 
 // * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western. -->
 
-const filmoteca = [
-  [
-    id,
-    titulo,
-    director,
-    añoEstreno,
-    paisOrigen,
-    género,
-    calificacion
- ],
-  [
-    id,
-    titulo,
-    director,
-    añoEstreno,
-    paisOrigen,
-    género,
-    calificacion
-  ],
-  [
-    id,
-    titulo,
-    director,
-    añoEstreno,
-    paisOrigen,
-    género,
-    calificacion
-  ]
+// Solocion J.M.
 
-]
-
-const generosDisponibles = [
-  'Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film Noir', 'Game'-'Show', 'History', 'Horror', 'Musical', 'Music', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk'-'Show', 'Thriller', 'War', 'Western'
-]
+// const generosDisponibles = 
 
 class Pelicula {
   constructor ({
     id,
     titulo,
     director,
-    añoEstreno,
+    estreno,
     paisOrigen,
-    género,
+    genero,
     calificacion
   })
 
-  {this.id = id
-  this.titulo = titulo
-  this.director = director
-  this.añoEstreno = añoEstreno
-  this.paisOrigen = paisOrigen
-  this.género = género
-  this.calificacion = calificacion}
+  {
+    this.id = id
+    this.titulo = titulo
+    this.director = director
+    this.estreno = estreno
+    this.paisOrigen = paisOrigen
+    this.genero = genero
+    this.calificacion = calificacion
 
-  static generos () {
-    
+    this.validarIMDB(id)
+    this.validarTitulo(titulo)
+    this.validarDirector(director)
+    this.validarAñoEstreno(estreno)
+    this.validarPais(paisOrigen)
+    this.validarGeneros(genero)
+    this.validarCalificacion(calificacion)
+  }
+
+  static get listaGeneros () {
+    return [
+      'Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film Noir', 'Game-Show', 'History', 'Horror', 'Musical', 'Music', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western']
   }  
 
-  ficha () {
-    console.log(`
-      Id: ${this.id}
-      Titulo: ${this.titulo}
-      Director: ${this.director}
-      Año de Estreno: ${this.añoEstreno}
-      Pais de Origen: ${this.paisOrigen}
-      Género: ${this.género} 
-      Calificación: ${this.calificacion} 
+  static generosAceptados() {
+    return console.info(`Los generos aceptados son: ${Pelicula.listaGeneros.join(', ')}`)
+  }
+
+  fichaTecnica() {
+    console.info(`
+Ficha Técnica:
+  Id = ${this.id}
+  Titulo = ${this.titulo}
+  Director = ${this.director}
+  Estreno = ${this.estreno}
+  Pais de Origen = ${this.paisOrigen.join('-')}
+  Genero = ${this.genero.join('/')}
+  Calificacion = ${this.calificacion}
     `)
   }
+
+  validarCadenas(propiedad, valor) {
+    if (!valor) return console.log(`${propiedad} "${valor}" está vaciío`)
+    if (typeof valor !== 'string') return console.log(`${propiedad} "${valor}" ingresado NO es una cadena de texto`)
+
+    return true
+  }
+
+  validarLoingitudCadena( propiedad, valor, longitud) {
+    if (valor.length > longitud) return console.log(`${propiedad} "${valor}" el tamaño del valor no cohincide con los valores permitidios (${longitud})`)
+
+    return true
+  }
+  
+  validarNumero(propiedad, numero) {
+    if (!numero) return console.log(`${propiedad} "${numero}" está vacío`)
+    if (typeof numero !== 'number') return console.log(`${propiedad} "${numero}" ingresado NO es un número`)
+
+    return true
+  }
+
+  validarArreglo(propiedad, valor) {
+    if (!valor) return console.log(`${propiedad} "${valor}" está vacío`)
+    if (!(valor instanceof Array)) return console.log(`${propiedad} "${valor}" ingresado NO es un arreglo`)
+    if (valor.length === 0) return console.log(`${propiedad} "${valor}" no tiene datos`)
+
+    for (let cadena of valor) {
+      if (typeof cadena !== 'string') return console.log(`El valor "${cadena}" NO es una cadena de texto`)
+    }
+    
+    return true
+  }
+
+  
+  validarIMDB (id) { 
+    if (this.validarCadenas('IMBD id', id)) {
+      if (!(/^([a-z]){2}([0-9]){7}$/.test(id))) {
+        return console.log( `IMDB id "${id}" no es válido, debe tener 9 caracteres, 2 letras minúsculas al iniciar seguido de 7 números`)
+      }
+    }
+  }
+
+  validarTitulo (titulo) {
+    if (this.validarCadenas('Titulo', titulo)) {
+       this.validarLoingitudCadena('Título', titulo, 100)
+    }
+  }
+
+  validarDirector (director) {
+    if (this.validarCadenas('Director', director)) {
+       this.validarLoingitudCadena('Director', director, 50)
+    }
+  }
+
+  validarAñoEstreno (estreno) {
+    if (this.validarNumero('Año de estreno', estreno)) {
+      if (!(/^([0-9]){4}$/.test(estreno))) {
+        return console.log( `Año de estreno "${estreno}" no es válido, debe tener 4 números`)
+      }
+    }
+  }
+  
+  validarPais(paisOrigen) {
+    this.validarArreglo('Pais', paisOrigen)
+  }
+
+  validarGeneros(genero) {
+    if (this.validarArreglo('Género', genero)) {
+      for (let generoUn of genero) {
+        if (!Pelicula.listaGeneros.includes(generoUn)) {
+         console.log(`Generos incorrectos "${genero.join(', ')}"`)
+          Pelicula.generosAceptados()
+        }
+      }
+    }
+  }
+
+  validarCalificacion (calificacion) {
+    if (this.validarNumero('Calificación', calificacion)) {
+      return (calificacion < 0 || calificacion > 10)
+        ? console.info(`La calificacion tiene que estar en un rango entre 0 y 10`)
+        : this.calificacion = calificacion.toFixed(1)
+      }
+    }
 }
+
+const peli = new Pelicula({
+  id: "tt1234567",
+  titulo: "Titulo de la peli",
+  director: "Directorde la peli",
+  estreno: 2234,
+  paisOrigen: ['México','Francia'],
+  genero: ['Adult', 'Drama'],
+  calificacion: 9.7
+})
+
+peli.fichaTecnica()
